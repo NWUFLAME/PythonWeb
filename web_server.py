7052db7756664e534142f3f059c3cf4ad69e19b6
NWUFLAME <39093352+NWUFLAME@users.noreply.github.com>
2019-04-22 15:45:32 +0800

    Add files via upload

---
+++ web_server.py
@@ -0,0 +1,92 @@
+import socket
+import re
+import sys
+
+import gevent
+from gevent import monkey
+
+monkey.patch_all()
+
+method = None
+
+
+class WSGIServer(object):
+    def __init__(self, port):
+        with open('web_server.conf') as f:
+            conf_dict = eval(f.read())
+            self.static_path = conf_dict['static_path']
+            self.router_suffix = conf_dict['router_suffix']
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind(('', port))
+        self.server_socket.listen(128)
+
+
+    def set_response_headers(self, status, headers):
+        self.status = status
+        self.headers = headers
+
+
+    def serve(self, client_socket):
+        content = client_socket.recv(1024)
+        if content:
+            content = content.decode('utf-8')
+            request_lines = content.splitlines()
+            request_url = re.search(r'(/\S*)', request_lines[0]).group(1)
+            print(request_url)
+            d = dict()
+            if '?' in request_url:
+                index = request_url.index('?')
+
+                params = request_url[index + 1:]
+                params = params.split('&')
+
+                for param in params:
+                    key, value = param.split('=')
+                    d[key] = value
+                request_url = request_url[:index]
+            if request_url == '/':
+                request_url = '/index.py'
+
+            try:
+                response = 'HTTP/1.1 200 OK\r\n\r\n'
+                if request_url.endswith(self.router_suffix):
+                    env = dict(url=request_url, params=d)
+                    html_content = method(env, self.set_response_headers).encode('utf-8')
+                    response = 'HTTP/1.1 %s\r\n' % self.status
+                    for header in self.headers:
+                        response += '%s: %s\r\n' % (header[0], header[1])
+                    response += '\r\n'
+                else:
+                    with open(self.static_path + request_url, 'rb') as f:
+                        html_content = f.read()
+            except Exception as e:
+                print(e)
+                response = 'HTTP/1.1 404 NOT FOUND\r\nContent-Type: text/html; charset=utf-8\r\n\r\n<h1>你要的内容不存在!</h1>'
+                client_socket.send(response.encode('utf-8'))
+            else:
+                client_socket.send(response.encode('utf-8'))
+                client_socket.send(html_content)
+        else:
+            print('客户端请求断开连接')
+        client_socket.close()
+
+
+    def run(self):
+        while True:
+            client_socket, client_addr = self.server_socket.accept()
+            gevent.spawn(self.serve, client_socket)
+        server_socket.close()
+
+
+def main(port):
+    server = WSGIServer(port)
+    global method
+    framework = __import__("wsgi")
+    method = getattr(framework, "mapper")
+    server.run()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(int(sys.argv[1]))
---
+++ wsgi.py
@@ -0,0 +1,78 @@
+import re
+import time
+import urllib.parse
+
+from pymysql import *
+
+url_mapping = dict()
+
+#数据库事务支持
+def dao_transaction(db, sql, params):
+    conn = connect(host='localhost', port=3306, database=db, user='root', password='123456', charset='utf8')
+
+    cs = conn.cursor()
+
+    res = cs.execute(sql, params)
+
+    conn.commit()
+    # 关闭Cursor对象
+    cs.close()
+    # 关闭Connection对象
+    conn.close()
+    return res
+
+#查询所有
+def fetch_all(db, sql, params):
+    conn = connect(host="localhost", port=3306, user='root', password='123456', database=db, charset='utf8')
+    cs = conn.cursor()
+    cs.execute(sql, params)
+    records = cs.fetchall()
+
+    cs.close()
+    conn.close()
+    return records
+
+#查询一条
+def fetch_one(db, sql, params):
+    conn = connect(host="localhost", port=3306, user='root', password='123456', database=db, charset='utf8')
+    cs = conn.cursor()
+    cs.execute(sql, params)
+    record = cs.fetchone()
+
+    cs.close()
+    conn.close()
+    return record
+
+
+#路由装饰器
+def route(pre):
+    def regist(func):
+        url_mapping[pre] = func
+
+        def wrapped_func(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapped_func
+
+    return regist
+
+#自定义的处理器
+@route(r'/index\.py')
+def index(ret):
+    return "xixi"
+
+#处理器映射器
+def mapper(env, start_response):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/html;charset=UTF-8')]
+    start_response(status, response_headers)
+    for u in url_mapping:
+        ret = re.match(u, env['url'])
+        if ret:
+            try:
+                return url_mapping[u](ret)
+            except Exception as e:
+                print(e)
+                return "系统异常，请稍后再试"
+        else:
+            return "请求的路径无效!"
